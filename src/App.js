7dc02226f1f486c9567d83888d3633388163cd53
Nicolas Cortes <johan.cortes@mail.escuelaing.edu.co>
2020-09-02 14:11:14 -0500

    parte 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+import {TodoApp} from "./TodoApp";
+import {Login} from "./Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {isLoggedIn: false, ruta:''};
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+
+
+
+    render() {
+            const LoginView = () => (
+                <Login clickHandler={this.clickHandler}/>
+            );
+            const TodoAppView = () => (
+                <TodoApp/>
+            );
+            if (this.state.isLoggedIn){
+                this.state.ruta = TodoApp;
+            }
+            else{
+                this.state.ruta = LoginView;
+            }
+
+
+
+
+            return (
+                <Router>
+                    <div className="App">
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo"/>
+                            <h1 className="Aepp-title">TODO React App</h1>
+                        </header>
+                        <div>
+                            <Route exact path="/" component={this.state.ruta}/>
+                            <Route exact path="/Login" component={LoginView}/>
+                            <Route exact path="/Todo" component={TodoAppView}/>
+                        </div>
+                    </div>
+                </Router>
+            );
+        }
+
+        clickHandler (e) {
+        this.setState({isLoggedIn:true}) ;
+        }
+    }
 
 export default App;
---
+++ src/TodoApp.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {TodoList} from "./TodoList";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import moment from "moment";
+import {Login} from "./Login";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+export class TodoApp extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {items: [], text: '', priority: 0, dueDate: moment()};
+        this.handleTextChange = this.handleTextChange.bind(this);
+        this.handlePriorityChange = this.handlePriorityChange.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    render() {
+
+        return (
+            <div className="TodoApp">
+                
+            </div>
+        );
+    }
+
+    handleTextChange(e) {
+        this.setState({
+            text: e.target.value
+        });
+    }
+
+    handlePriorityChange(e) {
+        this.setState({
+            priority: e.target.value
+        });
+    }
+
+    handleDateChange(date) {
+        this.setState({
+            dueDate: date
+        });
+    }
+
+    handleSubmit(e) {
+
+        e.preventDefault();
+
+        if (!this.state.text.length || !this.state.priority.length || !this.state.dueDate)
+
+            return;
+
+        const newItem = {
+            text: this.state.text,
+            priority: this.state.priority,
+            dueDate:moment(this.state.dueDate.toDateString())
+
+        };
+        this.setState(prevState => ({
+            items: prevState.items.concat(newItem),
+            text: '',
+            priority: '',
+            dueDate: ''
+        }));
+    }
+
+}
+
+export default TodoApp;
+
